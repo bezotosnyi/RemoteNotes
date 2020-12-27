435497c67fdf97261e1d7bf06d5a001ee06ae855
Dmytro <dima.bezotosnyi@gmail.com>
2020-12-27 20:23:48 +0200

    Added Service-Client contract

---
+++ src/RemoteNotes.Service.Client/RemoteNotes.Service.Client.Contract/IServiceClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using RemoteNotes.Service.Domain.DTO;
+
+namespace RemoteNotes.Service.Client.Contract
+{
+    public interface IServiceClient
+    {
+        UserDTO Login(string login, string password);
+        UserDTO RegisterUser(UserDTO user);
+
+        NoteDTO AddNote(NoteDTO note);
+        NoteDTO EditNote(NoteDTO note);
+        bool DeleteNote(int noteId);
+        List<NoteDTO> GetNotes(int accountId);
+    }
+}
