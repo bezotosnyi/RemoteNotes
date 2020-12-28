61a5bf63c25e98360acf80340f8daca01bad75b6
Dmytro <dima.bezotosnyi@gmail.com>
2020-12-29 00:33:45 +0200

    Added base projects to service

---
+++ src/RemoteNotes.Service/RemoteNotes.Service.Front.Host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemoteNotes.Service.Front.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
