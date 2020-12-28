c3555a2ddbd935636999c148a54c1666148a5cd1
Dmytro <dima.bezotosnyi@gmail.com>
2020-12-29 01:10:38 +0200

    Added Db context

---
+++ src/RemoteNotes.Service/RemoteNotes.DAL/RemoteNotesDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RemoteNotes.DAL.Core.Entities;
+
+namespace RemoteNotes.DAL
+{
+    public class RemoteNotesDbContext : DbContext
+    {
+        public RemoteNotesDbContext(DbContextOptions<RemoteNotesDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+
+        public virtual DbSet<Note> Notes { get; set; }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
