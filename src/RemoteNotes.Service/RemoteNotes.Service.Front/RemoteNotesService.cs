bc964876b2ff3f3ffc6e006690ec2b60e5e990ab
bezotosnyi <dima.bezotosnyi@gmail.com>
2020-12-29 12:30:35 +0200

    Added empty RemoteNotesService

---
+++ src/RemoteNotes.Service/RemoteNotes.Service.Front/RemoteNotesService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using RemoteNotes.Service.Domain.DTO;
+using RemoteNotes.Service.Domain.Helpers;
+using RemoteNotes.Service.Front.Contract;
+
+namespace RemoteNotes.Service.Front
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class RemoteNotesService : IRemoteNotesService
+    {
+        public OperationStatusInfo<UserDTO> Login(string login, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationStatusInfo<UserDTO> Registration(UserDTO user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationStatusInfo<NoteDTO> AddNote(NoteDTO note)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationStatusInfo<NoteDTO> EditNote(NoteDTO note)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationStatusInfo<bool> DeleteNote(int noteId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationStatusInfo<List<NoteDTO>> GetNotes(int accountId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
