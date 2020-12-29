d4c4bdfa5507b2bf3e07e48ce48e31e37beff52e
bezotosnyi <dima.bezotosnyi@gmail.com>
2020-12-29 11:42:42 +0200

    Made AttachedObject as generic

--- src/RemoteNotes.Service/RemoteNotes.Service.Domain/Helpers/OperationStatusInfo.cs
+++ src/RemoteNotes.Service/RemoteNotes.Service.Domain/Helpers/OperationStatusInfo.cs
@@ -1,11 +1,11 @@
 ﻿namespace RemoteNotes.Service.Domain.Helpers
 {
-    public class OperationStatusInfo
+    public class OperationStatusInfo<T>
     {
         public OperationStatus OperationStatus { get; set; }
 
         public string AttachedInfo { get; set; }
 
-        public object AttachedObject { get; set; }
+        public T AttachedObject { get; set; }
     }
 }
